17e613025fa80a6f48513674f7d9482cb7f43ef2
Elenilson <elenilsonsantos9@gmail.com>
2022-08-02 22:20:51 -0300

    exercicios resolvidos

---
+++ script.js
@@ -0,0 +1,45 @@
+// let arrayString = ["goiaba", "abacaxi", "acerola"]
+// let arrayNumber = [3, 7, 5]
+// let arraySortido= ["amora", 12,false]
+// let arrayNum = [20]
+// let arrayVazio = []
+
+// console.log(arrayString.length)
+// console.log(arrayString[3])
+// console.log(arrayString)
+// console.log(arraySortido.includes (12))
+// console.log(arrayNum)
+// console.log(arrayVazio)
+
+/*let copiaArrayString = arrayString
+console.log(copiaArrayString)
+
+copiaArrayString.push("uva")
+console.log(copiaArrayString)
+console.log(arrayString)*/
+
+// let copiaArrayString = arrayString.slice()
+// console.log(copiaArrayString)
+
+// copiaArrayString.push("uva")
+// console.log("original", arrayString)
+// console.log("copia", copiaArrayString)
+
+// copiaArrayString.splice(2,2)
+// console.log(copiaArrayString)
+
+// arrayString.sort()
+// console.log(arrayString)
+// arrayNumber.sort()
+// console.log(arrayNumber)
+arrayNumero2 = [4,6,2,5]
+console.log(arrayNumero2)
+
+arrayNumero2.sort()
+console.log(arrayNumero2)
+console.log(arrayNumero2.reverse())
+
+arrayNumero2.pop()
+arrayNumero2.push(6)
+arrayNumero2.splice(2,1)
+console.log(arrayNumero2)
